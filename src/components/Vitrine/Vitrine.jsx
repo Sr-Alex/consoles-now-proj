6c1ad178b98f746e841588bd889e9cb5094b5009
Alexandre Ricardo <alexandre.n314159@gmail.com>
2024-03-08 22:11:06 -0300

    Carrossel da vitrine finalizado.

--- src/components/Vitrine/Vitrine.jsx
+++ src/components/Vitrine/Vitrine.jsx
@@ -5,30 +5,34 @@ import ProductVitrine from "./ProductVitrine";
 import ArrowIcon from "../../assets/icons/ArrowIcon";
 
 function Vitrine({ products }) {
-  const wrapper = useRef();
+  const carousel = useRef(null);
 
   const scroll = (to = 0) => {
-    wrapper.current.scrollTo(wrapper.scrollLeft + 300, 0)
-    console.log(to);
-    console.log(wrapper.current.scrollLeft);
-  }
+    carousel.current.scrollTo(carousel.current.scrollLeft + to, 0);
+  };
+
+  console.log("Vitrine renderizada!");
 
   return (
     <section className="relative">
       <ul
-        ref={wrapper}
+        ref={carousel}
         className="scroll-hidden flex scroll-smooth snap-mandatory snap-x w-screen h-48 overflow-x-auto bg-proj-lightGray dark:bg-proj-darkGray"
       >
         {products.map((prod) => (
           <ProductVitrine key={prod.id} product={prod} />
         ))}
       </ul>
-      <button className="absolute left-4 bottom-1/2 translate-y-1/2 navArrowButton flex items-center justify-center"
-      onClick={() => scroll(-300)}>
+      <button
+        className="absolute left-4 bottom-1/2 translate-y-1/2 navArrowButton flex items-center justify-center"
+        onClick={() => scroll(-300)}
+      >
         <ArrowIcon className="w-6 h-6 fill-proj-darkGray -scale-x-100" />
       </button>
-      <button className="absolute right-4 bottom-1/2 translate-y-1/2 navArrowButton flex items-center justify-center"
-      onClick={() => scroll(300)}>
+      <button
+        className="absolute right-4 bottom-1/2 translate-y-1/2 navArrowButton flex items-center justify-center"
+        onClick={() => scroll(300)}
+      >
         <ArrowIcon className="w-6 h-6 fill-proj-darkGray" />
       </button>
     </section>
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
 
-import Vitrine from "../components/Vitrine/Vitrine";
+import Vitrine from "../components/Vitrine/Vitrine.jsx";
 
 function Homepage() {
-  const [ topProductList, setTopProductList] = useState([
+  const [productList, setProductList] = useState([
     {
       id: 1,
       name: "PlayStation 5",
       price: 3500,
-      imgURL: "https://infostore.vteximg.com.br/arquivos/ids/250371-0-0/console_playstation_5_slim_1tb_ssd_spider_man_2_-_sony_02.jpg?v=638362868111200000"
+      imgURL:
+        "https://infostore.vteximg.com.br/arquivos/ids/250371-0-0/console_playstation_5_slim_1tb_ssd_spider_man_2_-_sony_02.jpg?v=638362868111200000",
     },
     {
       id: 2,
       name: "Xbox Series X",
       price: 3500,
-      imgURL: "https://target.scene7.com/is/image/Target/GUEST_350eb4ce-48a6-4503-9e6c-f59212b6b584?wid=800&hei=800&qlt=80&fmt=webp"
-    }
+      imgURL:
+        "https://target.scene7.com/is/image/Target/GUEST_350eb4ce-48a6-4503-9e6c-f59212b6b584?wid=800&hei=800&qlt=80&fmt=webp",
+    },
+    {
+      id: 3,
+      name: "Nintendo Switch",
+      price: 25000,
+      imgURL:
+        "https://a-static.mlcdn.com.br/800x560/nintendo-switch-32gb-hac-001-01-1-controle-joy-con-vermelho-e-azul/magazineluiza/043082000/d38296e874a7d5f3e06ff0d8704bb123.jpg",
+    },
   ]);
 
   return (
     <main className="">
-      <Vitrine products={topProductList} />
+      <Vitrine products={productList} />
     </main>
   );
 }
